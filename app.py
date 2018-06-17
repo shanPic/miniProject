e4ed0ba4617ca21051659cf522ce90d98c81e180
shanPic <zhanshan333@outlook.com>
2018-06-18 01:16:34 +0800

    2018-6-18-1:16

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from PIL import Image
 
 app = Flask(__name__)
 
-def errorReturn(arg,result):
-    result['value'] = -1
+def errorReturn(arg,no,result):
+    result['value'] = no
     result['error'] = arg
     return jsonify(result)
 
@@ -20,18 +20,18 @@ def getScoreWithImg():
         #获取url中传递过来的参数，若无此参数，则返回一个包含错误信息字段，与value键值为-1的json
         benchmark = request.form.get('benchmark') #基准视频编号
         if benchmark == None:
-            return errorReturn('not have benchmark',result)
+            return errorReturn('not have benchmark',-1,result)
 
         timeStamp = request.form.get('timestamp') #图片时间戳
         if timeStamp == None:
-            return errorReturn('not have timestamp',result)
+            return errorReturn('not have timestamp',-2,result)
 
         try:
             imgOri = request.files.get('img')
             if(imgOri==None):
-                return errorReturn('the image is null',result)
+                return errorReturn('the image is null',-3,result)
         except:
-            return errorReturn('can\'t get image in this connect',result)
+            return errorReturn('can\'t get image in this connect',-4,result)
 
 
         imgComp = Image.open(imgOri)
@@ -41,10 +41,10 @@ def getScoreWithImg():
         # f.write(print(imgOri)
         # f.close()
 
-        try:
-            imgBenchmark = Image.open('/src/'+str(benchmark)+'/'+str(timeStamp)+'.jpg')
-        except:
-            return errorReturn('can\'t find the timeStmp in this benchmark video',result)
+        # try:
+        #     imgBenchmark = Image.open('/src/'+str(benchmark)+'/'+str(timeStamp)+'.jpg')
+        # except:
+        #     return errorReturn('can\'t find the timeStmp in this benchmark video',-5,result)
 
 
         score = randint(0,100)   #!TODO 此处调用后端算法接口得到分数
